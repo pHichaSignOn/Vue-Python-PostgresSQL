3bae41e19b4fb964a5d056656a9b79e3cc4c4c5c
pHichaSignOn <98305702+pHichaSignOn@users.noreply.github.com>
2026-04-01 21:48:20 +0700

    Add files via upload

---
+++ frontend/src/main.js
@@ -0,0 +1,23 @@
+/**
+ * main.js
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
+
+// Plugins
+import { registerPlugins } from '@/plugins'
+
+// style main.js
+import '@/styles/main.css';
+
+// Components
+import App from './App.vue'
+
+// Composables
+import { createApp } from 'vue'
+
+const app = createApp(App)
+
+registerPlugins(app)
+
+app.mount('#app')
